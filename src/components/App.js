cf42fceab37050da37573e499b8c5fc529ff90df
jeje rome <jege.roome@gmail.com>
2023-04-06 20:47:14 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import "../css/bootstrap.min.css";
+//import Arch from "./arch";
+//import "../css/style.css";
+//import "../css/fontawsome.all.min.css"
+
+
+
+export default function App() {
+
+
+  return (
+    <div>
+        Hello world
+    </div>
+  )
+}
